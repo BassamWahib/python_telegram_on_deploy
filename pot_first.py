ea4895ec1e57e8fee2951800dbaf84c7eac41ce5
BassamWahib <dr_bassam77@hotmail.com>
2023-12-08 19:16:25 +0200

    Update FastAPI script

--- pot_first.py
+++ pot_first.py
@@ -35,7 +35,7 @@ def read_root():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=5040)
 
 async def help(update, context):
     await context.bot.send_message(chat_id=update.effective_chat.id, text='Hi, I am an image manipulation program. To start click /start')
