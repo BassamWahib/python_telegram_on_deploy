f46f6d20880fbfe956aa63d36b72b1e40b7c6960
Bassam Wahib <120278186+BassamWahib@users.noreply.github.com>
2023-12-08 19:58:11 +0200

    Update pot_first_1.py

--- pot_first_1.py
+++ pot_first_1.py
@@ -30,7 +30,7 @@ def read_root():
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="127.0.0.1", port=5040)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
     # uvicorn pot_first:app --host 0.0.0.0 --port 10000
 
 
