35fdfc5d8e9d3054005ca3422ab47b37056a9d60
Bassam Wahib <120278186+BassamWahib@users.noreply.github.com>
2023-12-09 18:38:30 +0200

    Update pot_first.py

--- pot_first.py
+++ pot_first.py
@@ -3,37 +3,32 @@ from rembg import remove
 from PIL import Image
 
 import nest_asyncio
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
 from fastapi import UploadFile, HTTPException
 from fastapi import FastAPI, Form, File
+from telegram import Update
+
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  4 03:48:01 2023
+
+@author: Bassam Wahib
+"""
 
 
-TOKEN = "6705629015:AAEGa-In-23Vl-WsidDmU_qT1uZTRlwWo4"
 
-app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
-
-# Define your FastAPI routes and other code here
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run(app, host="127.0.0.1", port=8000)
-    # uvicorn pot_first:app --host 0.0.0.0 --port 10000
-
+TOKEN = "6705629015:AAEGa-InW-23Vl-WsidDmU_qT1uZTRlwWo4"
 
 async def help(update, context):
     await context.bot.send_message(chat_id=update.effective_chat.id, text='Hi, I am an image manipulation program. To start click /start')
 
 async def start(update, context):
     await context.bot.send_message(chat_id=update.effective_chat.id, text='To remove image background, please send your image')
-
-async def process_image(photo_name: str):
+    
+async def process_image(photo_name:str):
     name, _ = os.path.splitext(photo_name)
     output_photo_path = f'./processed/{name}.png'
     input = Image.open(f'./temp/{photo_name}')
@@ -41,42 +36,30 @@ async def process_image(photo_name: str):
     output.save(output_photo_path)
     os.remove(f'./temp/{photo_name}')
     return output_photo_path
-
+    
 async def handle_message(update, context):
     if filters.PHOTO.check_update(update):
         file_id = update.message.photo[-1].file_id
         unique_file_id = update.message.photo[-1].file_unique_id
         photo_name = f'{unique_file_id}.jpg'
-
+        
     elif filters.Document.IMAGE:
         file_id = update.message.document.file_id
         _, f_ext = os.path.splitext(update.message.document.file_name)
         unique_file_id = update.message.document.file_unique_id
         photo_name = f'{unique_file_id}.{f_ext}'
-
+        
     photo_file = await context.bot.get_file(file_id)
-    await photo_file.download_to_drive(custom_path=f'./temp/{photo_name}')
+    await photo_file.download_to_drive(custom_path = f'./temp/{photo_name}')
     await context.bot.send_message(chat_id=update.effective_chat.id, text='Processing your image')
     processed_image = await process_image(photo_name)
-    await context.bot.send_document(chat_id=update.effective_chat.id, document=processed_image)
+    await context.bot.send_document(chat_id=update.effective_chat.id, document = processed_image)
     os.remove(processed_image)
-
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    with open(f"./temp/{file.filename}", "wb") as f:
-        f.write(file.file.read())
-    return {"filename": file.filename}
-
-@app.get("/downloadfile/{filename}")
-async def read_item(filename: str):
-    file_path = f"./temp/{filename}"
-    if not os.path.exists(file_path):
-        raise HTTPException(status_code=404, detail="File not found")
-    return FileResponse(file_path, media_type="application/octet-stream", filename=filename)
+        
 
 if __name__ == '__main__':
     nest_asyncio.apply()
-
+    
     application = ApplicationBuilder().token(TOKEN).build()
 
     # Command handler
