5843be4036babd1c4e3ca40fe81f2c98564e5713
Bassam Wahib <120278186+BassamWahib@users.noreply.github.com>
2023-12-09 18:51:07 +0200

    Update pot_first_1.py

--- pot_first_1.py
+++ pot_first_1.py
@@ -81,4 +81,4 @@ if __name__ == '__main__':
     application.run_polling()
 
     # Run FastAPI with Uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
